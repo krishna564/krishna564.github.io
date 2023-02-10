ecd9d05a91c8088aef891f4c16bb3e219ec5962f
krishna564 <rbkteja@gmail.com>
2023-02-10 13:42:54 +0530

    Updates

--- backend/game/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import games
-
-class GamesAdmin(admin.ModelAdmin):
-    list_display = ("name", "url", "author", "published_date")
-
-admin.site.register(games, GamesAdmin)
--- backend/game/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class games(models.Model):
-    name = models.CharField(max_length=255)
-    url = models.TextField()
-    author = models.CharField(max_length=255)
-    published_date = models.DateTimeField()
